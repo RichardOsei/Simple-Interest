002894551f667f5f3512da166671bb0686d5939a
RichardOsei <59200416+RichardOsei@users.noreply.github.com>
2022-04-05 15:58:27 +0100

    Updated JS
    Checked for positive principal

--- script.js
+++ script.js
@@ -3,17 +3,27 @@ function calculate()
     p = document.getElementById("p").value;
     y = document.getElementById("y").value;
     r = document.getElementById("r").value;
+    i= (p*y*r/100);
     year = new Date().getFullYear()+parseInt(y);
     result = document.getElementById("result");
+
+
     
-    i= (p*y*r/100);
-    result.innerHTML = "If you deposit " + p + 
+    if(p <= 0){
+      result.innerHTML = "Enter a positive number";
+    }
+
+    else if(p==""){
+     result.innerHTML = "Enter a positive number";
+    }
+    else{
+    return result.innerHTML = "If you deposit " + p + 
                     " at an interest rate of " + r + "%"+
                     "</br>You will receive an amount of "+i+
                     "</br>in "+year+ " years";
+        }
 }
-
-var output = document.getElementById("demo");
+output = document.getElementById("demo");
 output.innerHTML = r.value;     
 r.oninput = function() {
   output.innerHTML = this.value;
